134148d654e6573ff617ad6a50a925d6598bcd53
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-09-06 16:26:45 +0300

    Solved problem G

---
+++ Product Analytics/Contest 1/G. multiplier_decomposition.py
@@ -0,0 +1,18 @@
+def prime_factors(n):
+    while n % 2 == 0:
+        print(2, end=' ')
+        n //= 2
+
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            print(i, end=' ')
+            n //= i
+        i += 2
+
+    if n > 2:
+        print(n)
+
+
+n = int(input())
+prime_factors(n)
