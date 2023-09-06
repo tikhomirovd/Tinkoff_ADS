eab4690b9d73b83dd56f1938bd26eb677ea9cb67
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-09-06 14:13:12 +0300

    Solved problem A

---
+++ Product Analytics/Contest 1/A. check_string.py
@@ -0,0 +1,9 @@
+def check_string(s):
+    if (s[0].isalpha() and s[1].isdigit()) or (s[0].isdigit() and s[1].isalpha()):
+        print("YES")
+    else:
+        print("NO")
+
+
+s = input()
+check_string(s)
