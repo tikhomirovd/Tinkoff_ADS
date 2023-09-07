ad3f992a813a58dc6625d197817658489c31dea5
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-09-07 20:44:56 +0300

    Renamed type

--- Python/Contest 1/H. palindrom.py
+++ Python/Contest 1/H. palindrom.py
@@ -7,14 +7,18 @@ def can_become_palindrome(s):
         return True
 
     # Проверяем подстроки s, начиная с первого символа и до последнего, затем до предпоследнего и так далее
-    for i in range(len(s), len(s) - 1):
-        if is_palindrome(s[i:]):
+    for i in range(len(s) - 1, 2, -1):
+        sub_s = s[:i]
+        if s[i] != "a":
+            break
+        if is_palindrome(sub_s):
             return True
 
     return False
 
 
 s = input()
+# s = "acbcaa"
 
 if can_become_palindrome(s):
     print("Yes")
