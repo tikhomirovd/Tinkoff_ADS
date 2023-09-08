fad19bc10ee810f2d82f5a0c9bdd555bee72397f
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-09-08 14:33:58 +0300

    Solved problem K

---
+++ Python/Contest 1/K. decompress.py
@@ -0,0 +1,21 @@
+def decompress(s):
+    decompressed = []
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+
+            num_start = i
+            while i + 1 < len(s) and s[i + 1].isdigit():
+                i += 1
+            repetitions = int(s[num_start:i + 1])
+
+            decompressed.append(s[i + 1] * repetitions)
+            i += 1
+        else:
+            decompressed.append(s[i])
+        i += 1
+    return ''.join(decompressed)
+
+
+compressed_string = input()
+print(decompress(compressed_string))
