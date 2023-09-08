a4d18b237916b2d1724e54924946a6ea93e45a6d
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-09-08 14:27:03 +0300

    Solved problem I

---
+++ Python/Contest 1/I. is_honestly_even.py
@@ -0,0 +1,14 @@
+def is_honestly_even(num):
+    for digit in str(num):
+        if int(digit) % 2 != 0:
+            return False
+    return True
+
+n = int(input())
+count = 0
+
+for i in range(1, n + 1):
+    if is_honestly_even(i):
+        count += 1
+
+print(count)
